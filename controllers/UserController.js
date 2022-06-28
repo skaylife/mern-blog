ba24dd6b60399aab7351386d3c128feeea340242
skaylife <worldskaynet@gmail.com>
2022-06-29 00:43:49 +0700

    Рефакторинг index.js и перенос всего кода в папку controllers (UserController.js)

---
+++ controllers/UserController.js
@@ -0,0 +1,125 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+
+import UserModel from '../models/User.js';
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+           return res.status(400).json(errors.array());
+        }
+       
+        // Создание зашифрованного паролья 
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+       
+        // Присвоение doc модели user с названием полей
+        const doc = new UserModel({
+           email: req.body.email,
+           fullName: req.body.fullName,
+           avatarUrl: req.body.avatarUrl,
+           passwordHash: hash,
+        })
+
+       // Сохранение user в базе данных 
+        const user = await doc.save();
+
+        // Создание Jwt token'a
+        const token = jwt.sign({
+            _id: user._id, // Зашифрока id - Для фиксации что в token'e есть id.
+        }, 
+        'secret123', // Щифрока сообщения 
+        {
+            expiresIn: '30d' // Время жизни токена.
+        }
+        ); 
+
+        // Деструктуризация для того чтоб не использовать passwordHash
+        const {passwordHash, ...userData} = user._doc; 
+       
+        // Успешный пуш данных для регистрации 
+        res.json({
+            ...userData,
+            token 
+        });
+    } catch (err) {
+            console.log('Ошибка при регистрации: ', err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться',
+        });
+    }   
+};
+
+export const login =  async (req, res) => {
+    try {
+        // Поиск в базе данных
+        const user = await UserModel.findOne({ email:  req.body.email }); 
+
+        // Проверка на случай если пользователь отсутсвует 
+        if(!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден',
+            });
+        }
+
+        // Сравнение паролей с хешем на валидность 
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль',
+            });
+        }
+
+        // Создание Jwt token'a
+        const token = jwt.sign({
+            _id: user._id, // Зашифрока id - Для фиксации что в token'e есть id.
+        }, 
+        'secret123', // Щифрока сообщения 
+            {
+                expiresIn: '30d' // Время жизни токена.
+            }
+        ); 
+
+        // Деструктуризация для того чтоб не использовать passwordHash
+        const {passwordHash, ...userData} = user._doc; 
+
+        // Успешный пуш данных для регистрации 
+        res.json({
+            ...userData,
+            token 
+        });
+
+    } catch (err) {
+        console.log('Ошибка авторизации: ', err);
+        res.status(500).json({
+            message: 'Не удалось авторизоваться',
+        });
+    }
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if(!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+
+        // Деструктуризация для того чтоб не использовать passwordHash
+        const {passwordHash, ...userData} = user._doc; 
+
+        // Успешный пуш данных для регистрации 
+        res.json(userData);
+    } catch (err) {
+        console.log('Ошибка авторизации: ', err);
+        res.status(500).json({
+            message: 'Нет доступа',
+        });
+    }
+};
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+
 import mongoose from 'mongoose';
 
 import { registerValidation } from './validations/auth.js';
-import { validationResult } from 'express-validator';
 
-import UserModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js'
 
 
 
@@ -20,126 +19,14 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', async (req, res) => {
-    try {
-        // Поиск в базе данных
-        const user = await UserModel.findOne({ email:  req.body.email }); 
+// Авторизация
+app.post('/auth/login', UserController.login);
 
-        // Проверка на случай если пользователь отсутсвует 
-        if(!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден',
-            });
-        }
-
-        // Сравнение паролей с хешем на валидность 
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Неверный логин или пароль',
-            });
-        }
-
-        // Создание Jwt token'a
-        const token = jwt.sign({
-            _id: user._id, // Зашифрока id - Для фиксации что в token'e есть id.
-        }, 
-        'secret123', // Щифрока сообщения 
-            {
-                expiresIn: '30d' // Время жизни токена.
-            }
-        ); 
-
-        // Деструктуризация для того чтоб не использовать passwordHash
-        const {passwordHash, ...userData} = user._doc; 
-
-        // Успешный пуш данных для регистрации 
-        res.json({
-            ...userData,
-            token 
-        });
-
-    } catch (err) {
-        console.log('Ошибка авторизации: ', err);
-        res.status(500).json({
-            message: 'Не удалось авторизоваться',
-        });
-    }
-});
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-           return res.status(400).json(errors.array());
-        }
-       
-        // Создание зашифрованного паролья 
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-       
-        // Присвоение doc модели user с названием полей
-        const doc = new UserModel({
-           email: req.body.email,
-           fullName: req.body.fullName,
-           avatarUrl: req.body.avatarUrl,
-           passwordHash: hash,
-        })
-
-       // Сохранение user в базе данных 
-        const user = await doc.save();
-
-        // Создание Jwt token'a
-        const token = jwt.sign({
-            _id: user._id, // Зашифрока id - Для фиксации что в token'e есть id.
-        }, 
-        'secret123', // Щифрока сообщения 
-        {
-            expiresIn: '30d' // Время жизни токена.
-        }
-        ); 
-
-        // Деструктуризация для того чтоб не использовать passwordHash
-        const {passwordHash, ...userData} = user._doc; 
-       
-        // Успешный пуш данных для регистрации 
-        res.json({
-            ...userData,
-            token 
-        });
-    } catch (err) {
-            console.log('Ошибка при регистрации: ', err);
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться',
-        });
-    }   
-});
+// Регистрация 
+app.post('/auth/register', registerValidation, UserController.register);
 
 // Информация о нас
-app.get('/auth/me',checkAuth, async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId);
-
-        if(!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            })
-        }
-
-        // Деструктуризация для того чтоб не использовать passwordHash
-        const {passwordHash, ...userData} = user._doc; 
-
-        // Успешный пуш данных для регистрации 
-        res.json(userData);
-    } catch (err) {
-        console.log('Ошибка авторизации: ', err);
-        res.status(500).json({
-            message: 'Нет доступа',
-        });
-    }
-})
+app.get('/auth/me',checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
     if(err) {
