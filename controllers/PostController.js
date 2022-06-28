929a92d47607ae61d6a6bff9452b73d1c7bda4c7
skaylife <worldskaynet@gmail.com>
2022-06-29 02:36:49 +0700

    Создание роутера и контроллера для удаления статьи

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,9 +2,56 @@ import PostModel from '../models/Post.js';
 
 export const getAll = async (req, res) => {
     try {
+
+        // Создание запроса для получения статьи 
         const posts = await PostModel.find().populate('user').exec();
 
+        // Получение статье 
         res.json(posts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось вывести статьи',
+        });
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+
+        // Создание запроса для получения статьи 
+        const postId = req.params.id;
+
+        // Поиск поста
+        PostModel.findOneAndUpdate(
+            {
+                _id: postId,
+            },
+            {
+                // Добавить один просмотр
+                $inc: { viewsCount: 1 }
+            },
+            {
+                // Возврат обновленной верси документа
+                returnDocument: 'after' 
+            },
+            (err, doc) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: 'Не удалось вернуть статью',
+                    });
+                }
+
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Статья не найдена'
+                    });
+                }
+
+                res.json(doc);
+            }
+        )
     } catch (err) {
         console.log(err);
         res.status(500).json({
@@ -31,6 +78,42 @@ export const create = async (req, res) => {
         console.log(err);
         res.status(500).json({
             message: 'Не удалось создать статью',
+        });
+    }
+}
+
+export const remove = async (req, res) => {
+    try {
+
+        // Создание запроса для получения статьи 
+        const postId = req.params.id;
+
+        PostModel.findOneAndDelete({
+            _id: postId,
+        }, (err, doc) => {
+            if(err) {
+                console.log(err);
+                res.status(500).json({
+                    message: 'Не удалось удалить статью',
+                });
+            }
+
+            if (!doc) {
+                return res.status(404).json({
+                    message: 'Статья не найдена',
+                })
+            }
+
+            res.json({
+                success: true,
+            });
+        })
+
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось вывести статьи',
         });
     }
 }
--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me',checkAuth, UserController.getMe);
 
 app.get('/posts', PostController.getAll);
-// app.get('/posts'/:id, PostController.getOne);
+app.get('/posts/:id', PostController.getOne);
 app.post('/posts',checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', PostController.remove);
+app.delete('/posts/:id', checkAuth, postCreateValidation, PostController.remove);
 // app.patch('/posts', PostController.update);
 
 
