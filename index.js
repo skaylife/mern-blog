09264c9191e496565944635100915949d897760d
skaylife <worldskaynet@gmail.com>
2022-06-29 22:15:03 +0700

    Refactoring index.js

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
-import checkAuth from './utils/checkAuth.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
 
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import { UserController, PostController } from './controllers/index.js'
+
  
 
 
